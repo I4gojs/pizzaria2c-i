c82bea584e82adf553b0432c2de02693f74c938c
I4gojs <iagojosesobrinho65@gmail.ocm>
2024-06-13 09:17:40 -0300

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import logo from '../assets/react.svg'
+import {Link} from 'react-router-dom'
+const Home = () =>{
+    return(
+        <img src={logo} width= "360"
+        alt="imagem do logo react"/>
+    )
+}
+
+export default Home
